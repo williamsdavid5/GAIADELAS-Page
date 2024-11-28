23922c9304d0f66b7f6f9db7ad13443ed08908b2
David Duarte (Williams) <davidduart04@gmail.com>
2024-11-28 09:31:34 -0300

    criacao e estilizacao do menu superior

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll", function () {
+
+    const scrollPos = window.scrollY;
+    const elemento = document.getElementById("efeitoMenu");
+
+    if (scrollPos > 2000) {
+        elemento.style.left = "520px";
+    } else if (scrollPos > 1200) {
+        elemento.style.left = "375px";
+    } else if (scrollPos > 200) {
+        elemento.style.left = "230px";
+    } else {
+        elemento.style.left = "100px";
+    }
+
+    if (scrollPos > 2600) {
+        elemento.style.opacity = 0;
+    } else {
+        elemento.style.opacity = 1;
+    }
+
+    const links = document.querySelectorAll(".menu-superior ul li a");
+
+    if (scrollPos > 1500) {
+        links.forEach(link => link.style.color = "rgb(0, 0, 0)");
+    } else {
+        links.forEach(link => link.style.color = "rgb(255, 255, 255)");
+    }
+
+});
